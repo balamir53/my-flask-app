b5d351d56fcf984c5b726cb3f480a66c2952bb3e
balamir53 <balamir53@yahoo.com>
2023-04-13 11:39:25 +0300

    items

--- resources/items.py
+++ resources/items.py
@@ -38,3 +38,31 @@ class Item(MethodView):
         except KeyError:
             abort(404,message='Item not found')
 
+
+@blp.route('/item')
+class ItemLists(MethodView):
+    def get(self):
+        '''
+        get all the items
+        '''
+        return {'items':list(items.values())}
+
+    def post(self):
+        '''
+        create item
+        '''
+        data = request.get_json()
+        if ( 'price' not in data
+            or 'store_id' not in data
+            or 'name' not in data):
+            abort(400, message= 'Lutfen verinin icinde price, store_id ve name oldugundan emin olun')
+        
+        for item in items.values():
+            if (data['name'] == item['name'] and
+                data['store_id'] == item['store_id']):
+                abort(400, message='Bu dukkanda zaten o malzeme var')
+        
+        item_id = uuid.uuid4().hex
+        new_item = {**data, 'id':item_id}
+        items[item_id] = new_item
+        return new_item, 201
